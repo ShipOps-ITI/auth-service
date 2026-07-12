bd117220819856b3aa711b7ba4a2fc31ce95be9c
Somaya Ayman <somaya189ayman@gmail.com>
2026-07-12 03:33:09 +0300

    fix server.js and add app . js

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+import userRoutes from "./src/routes/users.routes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+export default app;
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-import express from "express";
+import app from "./app.js";
 
-const app = express();
-    
+const PORT = process.env.PORT || 5000;
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
