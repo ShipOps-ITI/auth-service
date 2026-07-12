6a19279102b1f56a53f9dbc984bc6183581d1514
Somaya Ayman <somaya189ayman@gmail.com>
2026-07-12 12:21:44 +0300

    update user in prisma to include role and company id

---
+++ prisma/migrations/20260712092058_add_user_role/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Added the required column `role` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'FLEET_MANAGER', 'CUSTOMER', 'CAPTAIN', 'PORT_OPERATOR');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "companyId" INTEGER,
+ADD COLUMN     "role" "Role" NOT NULL,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
